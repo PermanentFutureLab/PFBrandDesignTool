08c2c4183543f3e1a13cb9d56d53511e1ae46020
Peter Willemsen <peter@codebuffet.co>
2016-08-23 16:30:12 +0200

    commit before leave

--- pflab-design-tool/js/editor/image-size.js
+++ pflab-design-tool/js/editor/image-size.js
@@ -3,8 +3,11 @@ $(function() {
   $("#set_image_size .ok").click(function() {
     var canvas = PF.editor.canvas;
 
+    // TODO: Scale objects as well (http://stackoverflow.com/questions/28301286/scale-fabric-js-canvas-objects)
+
     canvas.setWidth(parseInt($("#set_image_size #image_width").val()));
     canvas.setHeight(parseInt($("#set_image_size #image_height").val()));
     canvas.renderAll();
+    canvas.calcOffset();
   });
 });
