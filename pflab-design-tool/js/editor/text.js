d8909b1f366c10c5527f746d2d15e4e48b1cff72
Peter Willemsen <peter@codebuffet.co>
2016-08-23 16:02:55 +0200

    add text working

--- pflab-design-tool/js/editor/text.js
+++ pflab-design-tool/js/editor/text.js
@@ -1,5 +1,11 @@
 $(function() {
-  $('.add-text').click(function() {
-    
+  $('#add_text.modal .ok').click(function() {
+    var text = PF.editor.canvas.add(new fabric.Text($("#add_text.modal #text_content").val(), {
+        left: PF.editor.canvas.width / 2,
+        top: PF.editor.canvas.height / 2,
+        fill: 'black'
+    }));
+    PF.editor.canvas.renderAll();
+
   });
 });
