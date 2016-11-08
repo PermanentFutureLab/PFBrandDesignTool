1cb6150d2e56b8e1828c4950360e58c3de0552f1
Bart <bart@tuxion.nl>
2016-11-08 16:28:54 +0100

    update() Made default canvas width&height equal to A4 size

--- pflab-design-tool/js/editor/main.js
+++ pflab-design-tool/js/editor/main.js
@@ -2,4 +2,9 @@ PF.editor = {};
 
 $(function() {
   PF.editor.canvas = new fabric.Canvas('c');
+
+  // I got the pixel widths & height from http://superuser.com/posts/480196/revisions
+  let dimensions = { width: 2481/2, height: 3507/2 }
+  PF.editor.canvas.setWidth(dimensions.width).setHeight(dimensions.height);
+
 });
